f2b524d1a44afac46b505eaa68dbcebe44e7ef21
bob.fu <bob.fu@arcteryx.com>
2025-01-27 11:19:56 +0800

    submit

--- meetingroombooking/src/App.js
+++ meetingroombooking/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meeting Room Booking
         </a>
       </header>
     </div>
